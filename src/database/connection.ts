a6a267e622f3916a0e9f93ac22ae75fd2e6f052b
Easy Code <easycode002@gmail.com>
2024-09-20 09:38:04 +0700

    Added: Complete testing.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,7 +4,9 @@ import mongoose from "mongoose";
 async function connectToMongoDB() {
     try {
         await mongoose.connect(configs.mongodbUrl);
-        console.log(`MongoDB is connected!`)
+        console.log(`=========================================================`)
+        console.log(configs.mongodbUrl)
+        // console.log(`MongoDB is connected!`)
     } catch (error) {
         console.error(`connectToMongoDB() method error:`, error)
         throw error
